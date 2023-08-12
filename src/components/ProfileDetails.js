822a3252df0ff4dd8270848a50a49b98baf86361
Rahil Rashid <96280501+rahilrashid19@users.noreply.github.com>
2023-08-12 21:58:27 +0530

    changes

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -23,32 +23,61 @@ const ProfileDetails = ({
           </div>
         </div>
         <div className="contact-details flex  pt-20">
-          <div className="flex profile-details items-center justify-center flex-col border-b border-black">
-            <img
-              src={profilepicture}
-              alt={`Profile Pic of ${name}`}
-              className="w-52 h-52 rounded-full"
-            />
-            <h1 className="font-bold text-gray-600 text-2xl pt-4">{name}</h1>
-            <div className="name py-2">
-              <span className="text-gray-400 text-2xl">Username : </span>{" "}
-              <span className="text-gray-600 text-2xl font-bold">
-                {username}
-              </span>
+          <div className="contact-info flex flex-col">
+            <div className="flex profile-details items-center justify-center flex-col border-b border-black">
+              <img
+                src={profilepicture}
+                alt={`Profile Pic of ${name}`}
+                className="w-52 h-52 rounded-full"
+              />
+              <h1 className="font-bold text-gray-600 text-2xl pt-4">{name}</h1>
+              <div className="name py-2">
+                <span className="text-gray-400 text-2xl">Username : </span>{" "}
+                <span className="text-gray-600 text-2xl font-bold">
+                  {username}
+                </span>
+              </div>
+              <div className="Email py-2">
+                <span className="text-gray-400 text-2xl">Email : </span>{" "}
+                <span className="text-gray-600 text-2xl font-bold">
+                  {email}
+                </span>
+              </div>
+              <div className="phone py-2">
+                <span className="text-gray-400 text-2xl">Phone : </span>{" "}
+                <span className="text-gray-600 text-2xl font-bold">
+                  {phone}
+                </span>
+              </div>
+              <div className="website py-2">
+                <span className="text-gray-400 text-2xl">Website : </span>{" "}
+                <span className="text-gray-600 text-2xl font-bold">
+                  {website}
+                </span>
+              </div>
             </div>
-            <div className="Email py-2">
-              <span className="text-gray-400 text-2xl">Email : </span>{" "}
-              <span className="text-gray-600 text-2xl font-bold">{email}</span>
-            </div>
-            <div className="phone py-2">
-              <span className="text-gray-400 text-2xl">Phone : </span>{" "}
-              <span className="text-gray-600 text-2xl font-bold">{phone}</span>
-            </div>
-            <div className="website py-2">
-              <span className="text-gray-400 text-2xl">Website : </span>{" "}
-              <span className="text-gray-600 text-2xl font-bold">
-                {website}
-              </span>
+            <div className="company-details">
+              <h1 className="text-gray-400 text-3xl  flex justify-center pt-10">
+                Company
+              </h1>
+              <div className="website py-2">
+                <span className="text-gray-400 text-2xl">Name : </span>{" "}
+                <span className="text-gray-600 text-xl font-semibold">
+                  {company?.name}
+                </span>
+              </div>
+              <div className="website py-2">
+                <span className="text-gray-400 text-2xl">Catchphrase : </span>{" "}
+                <span className="text-gray-600 text-l font-semibold whitespace-normal">
+                  {company?.catchPhrase}
+                </span>
+              </div>
+              <div className="website py-2">
+                <span className="text-gray-400 text-2xl">Bs : </span>{" "}
+                <span className="text-gray-600 text-xl font-semibold">
+                  {company?.bs}
+                </span>
+              </div>
             </div>
           </div>
           <div className="address-details border-l border-black">
@@ -80,7 +109,6 @@ const ProfileDetails = ({
               </div>
             </div>
           </div>
-          <div className="map"></div>
         </div>
       </div>
     </>
